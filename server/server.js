cac25181237b6508259c8eb3c66d7e3fb0c881bf
Adam.s <adam.s@kinor.com>
2023-04-30 19:28:32 +0300

    added working exemple

--- server/server.js
+++ server/server.js
@@ -1,5 +1,19 @@
 const express = require('express');
 const app = express();
+var cors = require('cors');
+
+app.use(cors({
+	'Access-Control-Allow-Origin': '*'
+}
+));
+
+app.get('/', (req, res) => {
+	res.send('hello world');
+});
+
+
+
 app.listen(8496, () => console.log('listening at localhost:8496'));
-app.use(express.static('public'));
+
+
 
